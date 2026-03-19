2c5761c1dc2e232fdddf05af5f7e96900942068c
alexguzman23 <alexguz267@gmail.com>
2026-03-19 17:46:47 -0600

    Se agrega una retroalimentacion sobre los temas que vimos en la semana 9

---
+++ Semana8/RetroAlimentacionSemana9.py
@@ -0,0 +1,54 @@
+serie = "Fullmetal alchemits"
+
+## cada variable tiene un espacio de memoria asignado
+
+## mutabilidad= cuando una variable cambia se pierde la inmutabilidad
+## POO
+## polimorfismo -> es el cambio de acciones sin que se rompa el codigo
+## abstracciones ->
+## cafe coscafe
+## azucar
+## agua
+## otros ingredientes
+## un objeto es el que toma un modelo y este modelo le da funciones y utiliza sus propiedades
+
+## Leon ->
+## tiene ojos
+## tiene boca (propiedades)
+## esta guapo
+#############
+# corre (funiciones)
+# salta
+
+
+## Clases
+## EStructura de datos
+
+
+## es una variable que tiene adentro otra variable
+## Listas.
+## Arrays. -> se inicia a contar desde el 0
+## tuplas.
+## indices.
+
+
+# -------------------------------------------------------------------
+def saludo(nombres):
+    print(nombres)
+
+
+# saludo(serie) las funciones siempre an a tener parentesis ()
+# -------------------------------------------------------------------
+##las funciones tienen un espacio
+## Scope es donde residen las variables
+
+## Colocar el nombre de la serie como titulo
+fmatemu = serie.title()
+saludo(serie)
+# saludo(fmatemu)
+fmaMayusculas = serie.upper()
+saludo(fmaMayusculas)
+## de programacion Lineal
+
+FullmetalCapitalizer = serie.capitalize
+saludo(FullmetalCapitalizer)
