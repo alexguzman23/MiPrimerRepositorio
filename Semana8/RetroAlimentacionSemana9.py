7c5c58d224b9cea7864f735f042ac62bbb4304e9
alexguzman23 <alexguz267@gmail.com>
2026-03-19 20:01:40 -0600

    Retroalimentacion y clase semana 8-9

--- Semana8/RetroAlimentacionSemana9.py
+++ Semana8/RetroAlimentacionSemana9.py
@@ -49,6 +49,38 @@ saludo(serie)
 fmaMayusculas = serie.upper()
 saludo(fmaMayusculas)
 ## de programacion Lineal
+FullmetalCapitalizer = fmaMayusculas.swapcase().title()
+## cuando encadenamos funciones se indica que la salida de la funcion actual
+## 3 es la entrada de la siguiente funcion
 
-FullmetalCapitalizer = serie.capitalize
 saludo(FullmetalCapitalizer)
+
+## comparar cadenas de texto
+comparar1 = "Alex"
+comparar2 = "Alex "
+
+# casefold para comparar y pasar a minusculas
+variabletemporal = comparar1.casefold == comparar2.casefold()
+comparar = comparar1.casefold() == comparar2.casefold()
+# print(comparar)
+## casefold nos dara true unicamente si los elementos sin identicos sino nos dara un false
+
+# para verificar si es un numero o un caracter vamos a utilizar isalfa()
+clasicas2005 = "Gasolina 2025"
+comprarisAlpa = clasicas2005.isalpha()
+# print(comprarisAlpa, 2005)
+# isalpha nos va dar true si el string que se les esta enviando es unicamente letras
+
+## si lo que quiero es que sea solo numero isalnum
+
+letracancion = "Lo que paso, paso, entre tu y yo"  # no tengo numero
+decada = "10"
+
+ejemplo = letracancion.isalnum()
+print(ejemplo)
+ejemplo = decada.isalnum()
+# isalnum verifica si la cadena de texto solo tiene numeros si solo tiene numeros no vas a dar true
+# si tiene un solo espacio dara false
+
+# verificar que solo sean digitos
+comprobardecadas = decada
