2faafa11b88838d05d79203c1ef528921b20a907
alexguzman23 <alexguz267@gmail.com>
2026-04-07 16:43:31 -0600

    Ejercicio 1, Laboratorio 2

---
+++ Laboratorio2/Ejercicio1.py
@@ -0,0 +1,13 @@
+def transformar_texto(texto, opcion):
+
+    if opcion == 1:
+        return texto.upper()
+
+    elif opcion == 2:
+        return texto.lower()
+
+    elif opcion == 3:
+        return texto.capitalize()
+
+
+## Inge no dice que muestre algo por lo que supongo asi es sin print =)
