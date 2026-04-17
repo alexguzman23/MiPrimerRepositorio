bc0e6b6d862b105508a3c3d6f60dfc0677afe0b2
alexguzman23 <alexguz267@gmail.com>
2026-04-17 15:29:10 -0600

    Clase semana 10

---
+++ Semana10/Semana10.py
@@ -0,0 +1,17 @@
+## si la cadena de texto tiene  numeros
+siTiNumero = "Alex 2026"
+## este texto tiene numero -> True
+## isnumeric -> es un espacio en la memoria
+## isnumeric()-> dentro de la funcion
+respuesta = siTiNumero.isnumeric()
+print(respuesta)
+
+##isnumeric como numeros que van a estar ejecutandose desde una
+##cadena de texto
+
+isLowerCase = "Ella se fue me abandono y destrozo mi corazon"
+minusculas = isLowerCase.islower()
+print(minusculas)
+
+
+
