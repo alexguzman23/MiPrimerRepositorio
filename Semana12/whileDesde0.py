d48b53e6a3137f719819b5bebcc7bdffe612d876
alexguzman23 <alexguz267@gmail.com>
2026-04-17 15:35:04 -0600

    Uso de el While Semana 12 Clase

---
+++ Semana12/whileDesde0.py
@@ -0,0 +1,11 @@
+print("El while siempre lleva una orden logica")
+# > <  !=  and or not
+
+i = 0
+# dia del mes 27 de abril
+
+while i < 27:
+    print(f"Mes abril fecha {i}")
+    i += 1
+## i += 1
+## i = i + 1
